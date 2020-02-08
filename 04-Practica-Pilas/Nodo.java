1be1e057146d62de429e9d493d6e034c83a1e585
EusebioAjas <eusebioajas6@gmail.com>
2020-02-07 22:49:55 -0600

    Pilas

---
+++ 04-Practica-Pilas/Nodo.java
@@ -0,0 +1,25 @@
+public class Nodo {
+    private int data;
+    private Nodo next;
+
+    public Nodo(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+
+}
---
+++ 04-Practica-Pilas/Test.java
@@ -0,0 +1,16 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class Test {
+    public static final Scanner SC = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        menu();
+
+    }
+
+    public static void menu() {
+        System.out.printf("%40s\n", "MENU");
+    }
+
+}

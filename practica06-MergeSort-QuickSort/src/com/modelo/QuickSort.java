35d6f2ee36bb83af85cdfc62ab0bfd354c5125eb
EusebioAjas <eusebioajas6@gmail.com>
2020-03-17 20:41:33 -0600

    Eliminado comentarios

--- practica06-MergeSort-QuickSort/src/com/modelo/QuickSort.java
+++ practica06-MergeSort-QuickSort/src/com/modelo/QuickSort.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.modelo;
 
-/**
- *
- * @author cobhc
- */
 public class QuickSort {
     public static void sort(int[] array, int low, int high){
         
